9694656ff5b1f2491a93a29a7c90749373c44d34
Vitoria <vitoriaroas@gmail.com>
2021-12-13 15:43:39 -0500

    polished layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
-    <Layout>
-      <div className="container"></div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
     </Layout>
   );
 }
