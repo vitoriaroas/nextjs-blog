c8d82dc63672e47ed6bbc7c368f189579e707e44
Vitoria <vitoriaroas@gmail.com>
2021-12-14 14:37:45 -0500

    added remark-html

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,10 +5,12 @@ export default function Post({ postData }) {
   return (
     <Layout>
       {postData.title}
-      <br /> 
-      {postData.id} 
-      <br /> 
+      <br />
+      {postData.id}
+      <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
@@ -22,7 +24,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
