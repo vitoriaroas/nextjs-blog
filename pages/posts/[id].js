008f090e73d54bd088f9bfc88f4b13919587196f
Vitoria <vitoriaroas@gmail.com>
2021-12-14 14:12:05 -0500

    added a getStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,16 @@
 import Layout from "../../components/layout";
-import { getAllPostIds } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br /> 
+      {postData.id} 
+      <br /> 
+      {postData.date}
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -13,4 +21,11 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({ params }) {}
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
